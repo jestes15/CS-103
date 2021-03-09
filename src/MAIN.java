18ddfa57aadbeac302df19f73aa28976626f9ac6
jestes15 <sagepup0620@gmail.com>
2021-03-08 23:40:18 -0600

    Finished Q1 and started on Q2

--- src/MAIN.java
+++ src/MAIN.java
@@ -8,38 +8,16 @@ public class MAIN {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
+        Questions.QuestionSplitter(1);
         System.out.print("What is the file name(not directory and including the .txt): ");
         String fileName = sc.next();
-        Q1(fileName);
-    }
+        Questions.Q1(fileName);
 
-    public static void Q1(String filename) throws FatalError, IOException {
-        try {
-            FileReader fileObj = new FileReader(System.getProperty("user.dir") + OsInfo() + filename);
-            HashMap<String, Integer> mainHash = new HashMap<>();
+        Questions.QuestionSplitter(2);
+        Questions.Q2(sc);
 
-            HashMap<String, Integer> retVal = programmingAssign2.Q1(mainHash, fileObj);
-            for (java.util.Map.Entry<String, Integer> stringIntegerEntry : retVal.entrySet()) {
-                System.out.printf("The string contains %s\n", stringIntegerEntry);
-            }
-        } catch (FileNotFoundException e) {
-            throw new FatalError("The file could not be found, please check and make sure " +
-                    "it is in the same directory as the java file", e);
-        }
-    }
+        Questions.QuestionSplitter(3);
 
-    public static String OsInfo()
-    {
-        String OSInfo;
-        String OS = System.getProperty("os.name").toLowerCase();
-        if (OS.equals("linux") || OS.equals("mac os x"))
-        {
-            OSInfo = "/";
-        }
-        else {
-            OSInfo = "\\";
-        }
-        return OSInfo;
     }
 }
 
@@ -49,8 +27,29 @@ class FatalError extends Exception {
     }
 }
 
-class programmingAssign2 {
-    public static HashMap<String, Integer> Q1(HashMap<String, Integer> mainHash, Reader fileObj) throws IOException {
+class Questions {
+    public static void QuestionSplitter(int question) {
+        System.out.println("#############");
+        System.out.printf("This is the start of question %d\n", question);
+        System.out.println("#############");
+    }
+
+    public static void Q1(String filename) throws FatalError, IOException {
+        try {
+            FileReader fileObj = new FileReader(System.getProperty("user.dir") + OsInfo() + filename);
+            HashMap<String, Integer> mainHash = new HashMap<>();
+
+            HashMap<String, Integer> retVal = Q1UtilFunction(mainHash, fileObj);
+            for (java.util.Map.Entry<String, Integer> stringIntegerEntry : retVal.entrySet()) {
+                System.out.printf("The string contains %s\n", stringIntegerEntry);
+            }
+        } catch (FileNotFoundException e) {
+            throw new FatalError("The file could not be found, please check and make sure " +
+                    "it is in the same directory as the java file", e);
+        }
+    }
+
+    public static HashMap<String, Integer> Q1UtilFunction(HashMap<String, Integer> mainHash, Reader fileObj) throws IOException {
         BufferedReader bufferedFileObj = new BufferedReader(fileObj);
         String main;
         while ((main = bufferedFileObj.readLine()) != null) {
@@ -68,5 +67,46 @@ class programmingAssign2 {
             }
         }
         return mainHash;
+    }
+
+    public static String OsInfo() {
+        String OSInfo;
+        String OS = System.getProperty("os.name").toLowerCase();
+        if (OS.equals("linux") || OS.equals("mac os x"))
+        {
+            OSInfo = "/";
+        }
+        else {
+            OSInfo = "\\";
+        }
+        return OSInfo;
+    }
+
+    public static void Q2(Scanner sc) {
+        HashMap<String, String[]> studentGrade = new HashMap<>();
+        String[] studentValues = new String[4];
+        for (int i = 0; i < 4; i++) {
+            String classNum = "class-" + (i + 1);
+            System.out.print("Please input the amount of credits hours for the class: ");
+            studentValues[0] = String.valueOf(sc.next().charAt(0));
+            System.out.print("Please input the letter grade (one character only): ");
+            studentValues[1] = String.valueOf(sc.next().charAt(0));
+            studentGrade.put(classNum, studentValues);
+        }
+
+        for (int studentClass = 1; studentClass < 5; studentClass++) {
+            String className = "class-" + studentClass;
+            String[] info = studentGrade.get(className);
+            switch (info[1]) {
+                case "A" -> {System.out.println("You have an A");}
+                case "B" -> {System.out.println("You have an B");}
+                case "C" -> {System.out.println("You have an C");}
+                case "D" -> {System.out.println("You have an D");}
+                case "F" -> {System.out.println("You have an F");}
+            }
+        }
+
+        System.out.print(studentGrade);
+
     }
 }
